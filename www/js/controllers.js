10826b2ec26884f2bd996c583b3e793d2cf44890
John Zhao <john@zondr.com>
2015-12-22 23:47:30 -0600

    convert from 24 hour time to 12 hour cycle

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -92,11 +92,15 @@ angular.module('keepup.controllers', [])
     var hours   = Math.floor(sec_num / 3600);
     var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
     var seconds = sec_num - (hours * 3600) - (minutes * 60);
+    var cycle = "AM"
 
-    if (hours   < 10) {hours   = "0"+hours;}
+    if (hours > 12) {cycle = "PM"; hours = hours - 12}
+
+    // if (hours   < 10) {hours   = "0"+hours;}
     if (minutes < 10) {minutes = "0"+minutes;}
     if (seconds < 10) {seconds = "0"+seconds;}
-    var time    = hours+':'+minutes;
+    
+    var time    = hours+':'+minutes + ' ' + cycle;
     return time;
   }
 
